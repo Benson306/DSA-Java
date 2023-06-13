a1fcce28d063940394d1fbf0288489b294d9c8d0
Benson Ndiwa <bnkimtai@gmail.com>
2023-06-14 00:13:09 +0300

    Stacks

--- src/Queues.java
+++ src/Queues.java
@@ -1,5 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Queues {
     public static void main(String[] args) { //FIFO
+        Queue<String> queue = new LinkedList<String>();
 
+        queue.offer("Karen");
+        queue.offer("Mike");
+        queue.offer("Donna");
+        queue.offer("Harvey");
+
+        System.out.println(queue.peek()); // See What's at the head of our queue
+
+        queue.poll();
+
+        System.out.println(queue.size()); // Size of a queue
+
+        System.out.println(queue);
     }
 }
